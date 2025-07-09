8244e024270bdf8d558cfebb225e27e934d32fca
joneldiablo <jon_eldiablo@hotmail.com>
2025-07-09 09:20:42 -0600

    add navigation components

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,5 +1,10 @@
 export { default as Action } from "./actions/action";
 export { default as Navigation } from "./navigation/navigation";
-  
-export * from "./actions/action";
-export * from "./navigation/navigation";
+export { default as BrandNavigation } from "./navigation/brand-navigation";
+export { default as Navbar } from "./navigation/navbar";
+export { default as SideNavigation } from "./navigation/side-navigation";
+
+export * from "./actions/action";export * from "./navigation/navigation";
+export * from "./navigation/brand-navigation";
+export * from "./navigation/navbar";
+export * from "./navigation/side-navigation";
